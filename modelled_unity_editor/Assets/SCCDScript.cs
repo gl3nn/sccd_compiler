3092b349aa394056c6a1308cfea682bbbf3ea387
gl3nn <glenncodes@gmail.com>
2015-04-25 16:20:12 +0200

    PlayerTank (modelled) can now shoot.

--- modelled_unity_editor/Assets/SCCDScript.cs
+++ modelled_unity_editor/Assets/SCCDScript.cs
@@ -30,27 +30,21 @@ public class SCCDScript : MonoBehaviour {
         this.controller.update(0);
     }
 
-    void generateInputEventsForAxis(string axis, string negative_event_down, string positive_event_down, string negative_event_up, string positive_event_up)
+    void inputKey(KeyCode key, string pressed_event, string relesed_event)
     {
-        if (Input.GetButtonDown(axis))
-        {
-            Debug.Log(Input.GetAxisRaw(axis));
-            this.controller.addInput(new sccdlib.Event(Input.GetAxisRaw(axis) < 0 ? negative_event_down : positive_event_down, "input"));
-        }
-        if (Input.GetButtonUp(axis))
-        {
-            Debug.Log(Input.GetAxisRaw(axis));
-            this.controller.addInput(new sccdlib.Event(Input.GetAxisRaw(axis) < 0 ? negative_event_up : positive_event_up, "input"));
-        }
+        if (Input.GetKeyDown(key))
+            this.controller.addInput(new sccdlib.Event(pressed_event, "input"));
+        if (Input.GetKeyUp(key))
+            this.controller.addInput(new sccdlib.Event(relesed_event, "input"));
     }
 
     void generateInputEvents() {
-        sccdlib.Event input_event;
-
-        this.generateInputEventsForAxis("Horizontal", "left-pressed", "right-pressed", "left-released", "right-released");
-        this.generateInputEventsForAxis("Vertical", "down-pressed", "up-pressed", "down-released", "up-released");
-        this.generateInputEventsForAxis("ArrowHorizontal", "arrow-left-pressed", "arrow-right-pressed", "arrow-left-released", "arrow-right-released");
-        this.generateInputEventsForAxis("ArrowVertical", "arrow-down-pressed", "arrow-up-pressed", "arrow-down-released", "arrow-up-released");
-
+        this.inputKey(KeyCode.W, "up-pressed", "up-released");
+        this.inputKey(KeyCode.S, "down-pressed", "down-released");
+        this.inputKey(KeyCode.A, "left-pressed", "left-released");
+        this.inputKey(KeyCode.D, "right-pressed", "right-released");
+        this.inputKey(KeyCode.LeftArrow, "arrow-left-pressed", "arrow-left-released");
+        this.inputKey(KeyCode.RightArrow, "arrow-right-pressed", "arrow-right-released");
+        this.inputKey(KeyCode.UpArrow, "arrow-up-pressed", "arrow-up-released");
     }
 }
